34dfde40658846cd5eafae613bd42b1379fdd971
jaeyz <jc.nuevas@klab.com>
2013-12-26 19:41:06 -0800

    Changed the character controls

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,20 +3,55 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+	private float speed = 6.0f;
+	private float jumpSpeed = 8.0f;
+	private float gravity = 20.0f;
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	private CharacterController controller;
+
+	public float Speed {
+		get {
+			return speed;
+		} set {
+			speed = value;
+		}
+	}
+
+	public float JumpSpeed {
+		get {
+			return jumpSpeed;
+		} set {
+			jumpSpeed = value;
+		}
+	}
+
 	void Start () {
-	
+		controller = GetComponent<CharacterController> ();
 	}
 
 	void Update() {
-	
+
+#if UNITY_STANDALONE || UNITY_EDITOR
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 1);
+#elif UNITY_ANDROID || UNITY_IPHONE && !UNITY_EDITOR
+		moveDirection = new Vector3(Input.gyro.userAcceleration.x, 0, 1);
+#endif
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+
+			
+		if (Input.GetButton ("Jump")) {
+			moveDirection.y = jumpSpeed;
+		}
+
+		// Apply gravity
+		//moveDirection.y -= gravity * Time.deltaTime;
+		
+		// Move the controller
+		controller.Move (moveDirection * Time.deltaTime);
+
 	}
 
-	void FixedUpdate () {
-		rigidbody.AddForce (Vector3.up * 2);
-
-		if (Input.GetKey (KeyCode.A))
-			rigidbody.AddForce (Vector3.left * 2, ForceMode.Impulse);
-		if (Input.GetKey (KeyCode.D))
-			rigidbody.AddForce (Vector3.right * 2, ForceMode.Impulse);
-	}
 }
