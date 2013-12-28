654c51f5ac47600c9a4986e37162b864bc66e8a2
jaeyz <jc.nuevas@klab.com>
2013-12-28 01:08:12 -0800

    Magnet Test

---
+++ Assets/Scripts/AttachObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachObject : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Boulder")
+			transform.parent = col.transform;
+	}
+
+
+}
