19ba321623e2475d25aacfea50ef5d88890795dc
jaeyz <jc.nuevas@klab.com>
2014-01-05 00:06:40 -0800

    Hazard Effects

--- Assets/Scripts/Powerups/HazardPowerup.cs
+++ Assets/Scripts/Powerups/HazardPowerup.cs
@@ -5,15 +5,26 @@ public class HazardPowerup : MonoBehaviour {
 
 	void OnTriggerEnter(Collider col) {
 		if (col.tag == "Boulder") {
+
+			GameObject explosionPrefab = (GameObject) Resources.Load("Explosion");
+
+			Transform effect = Instantiate(explosionPrefab, transform.position, Quaternion.identity) as Transform;
+
 			int childCount = col.transform.childCount;
+
 			if (childCount > 0) {
 				for(int x = 0; x < (childCount /2); x++ ) {
 					BoulderBehaviour.Instance.DecreaseBoulderSize();
+
 					Destroy(col.transform.GetChild(x).gameObject);
 					Destroy(gameObject);
 				}
 			}
 		}
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
 	}
 
 }
