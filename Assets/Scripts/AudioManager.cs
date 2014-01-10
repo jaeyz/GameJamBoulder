52265457b3de2bbee34ecd58035d6e4eaf17d645
jaeyz <jc.nuevas@klab.com>
2014-01-10 01:17:25 -0800

    Sound Assets

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoSingleton<AudioManager> {
+
+	public AudioClip MAIN_MENU;
+
+	public override void Init () {
+		if (MAIN_MENU == null)
+			MAIN_MENU = (AudioClip) Resources.Load("SoundsAssets/Summer");
+	}
+
+	public void PlayMusic (AudioClip a) {
+		if (audio != null) {
+			if (a != null) {
+				audio.clip = a;
+				audio.Play();
+			}
+		}
+	}
+ 
+
+
+}
